7a04d320e19ba2f7c28a1b762835c65c98935017
Loren Segal <lsegal@soen.ca>
2010-04-01 15:47:23 -0400

    Fix inheritance logic

--- lib/export.rb
+++ lib/export.rb
@@ -6,12 +6,16 @@ module Export
   end
   
   def export(meth, *classes)
-    meth = meth.to_s
+    meth, expmeth = meth.to_s, "__exported__#{meth}"
     (export_list[meth] ||= []).push(*classes)
-    alias_method "__exported__#{meth}", meth
+    alias_method expmeth, meth
+    private expmeth
+    public meth
     define_method(meth) do |*args, &block|
       cclass, classes = caller_class, self.class.export_list[meth]
-      if classes && !classes.any? {|k| cclass >= k }
+      if cclass == false
+        raise NoMethodError, "`#{meth}' is not accessible outside #{self.class}"
+      elsif cclass != self.class && classes && !classes.any? {|k| cclass <= k }
         raise NoMethodError, 
           "`#{meth}' is not accessible to #{cclass.inspect}:#{cclass.class.inspect}",
           caller
@@ -19,6 +23,8 @@ module Export
       send("__exported__#{meth}", *args, &block)
     end
   end
+  
+  alias friend export
 end
 
 (RUBY_VERSION >= "1.9.1" ? BasicObject : Object).send(:extend, Export)
--- test/export_test.rb
+++ test/export_test.rb
@@ -4,8 +4,10 @@ require File.dirname(__FILE__) + '/../lib/export'
 class A; def bar; D.new.foo end end
 class B; def bar; D.new.foo end end
 class C; def bar; D.new.foo end end
+class E < B; def bar; D.new.foo end end
 class D
-  def foo; "foobar" end 
+  def foo; "foobar" end
+  def bar; foo end
   export :foo, A, B
 end
 
@@ -20,5 +22,13 @@ class TestCallsite < Test::Unit::TestCase
 
   def test_export_C
     assert_raises(NoMethodError) { C.new.bar }
+  end
+  
+  def test_export_E
+    assert_equal "foobar", E.new.bar
+  end
+  
+  def test_local_call
+    assert_equal "foobar", D.new.bar
   end
 end
