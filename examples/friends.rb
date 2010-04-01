7b5e19e913e2cd8714c6ecd2b168560b0da32937
Loren Segal <lsegal@soen.ca>
2010-04-01 15:47:38 -0400

    Add C++-like friend example

---
+++ examples/friends.rb
@@ -0,0 +1,35 @@
+require File.dirname(__FILE__) + '/../lib/export'
+
+class Car
+  def initialize; @engine = Engine.new end
+  
+  def turn_on(key)
+    if key == "foo"
+      @engine.engage
+      puts "Car turned on!"
+    else
+      puts "Wrong key!"
+    end
+  end
+end
+
+class Engine
+  private
+  def engage; puts "Engine turned on!" end
+  friend :engage, Car
+end
+
+# We can enable the engine if we 
+# turn the car on with the right key
+car = Car.new
+car.turn_on("foo")
+
+# But we can't enable the engine directly
+car.instance_variable_get("@engine").engage
+
+# Output:
+# Engine turned on!
+# Car turned on!
+# export.rb:17:in `block in export': `engage' is not accessible outside 
+# Engine (NoMethodError)
+# 	from friends.rb:35:in `<main>'
